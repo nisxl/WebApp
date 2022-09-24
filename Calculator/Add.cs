f9ba17c8b0e2b49dce2dae98b085a5b7c3f241cf
Nischal Maharjan <nis.maharaj@gmailcom>
2022-09-24 17:51:21 +0545

    Add project files.

---
+++ Calculator/Add.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Calculator
+{
+    internal class Add
+    {
+        public void Addition() {
+            var num1 = Console.ReadLine();
+            var num2 = Console.ReadLine();
+
+            int add = Convert.ToInt32(num1) + Convert.ToInt32(num2);
+
+            Console.WriteLine($"the sum is: {add}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1.Calculator;
+
+Add add = new Add();
+ add.Addition();
+Console.WriteLine("Hello, World!");
